8ed1b46d06cda5eafde46090bda0d33619e0f3ca
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2021-12-08 23:26:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ app = Client(
     api_hash=api_hash
 )
 @client.on(events.NewMessage(pattern="^/start$"))
-async def start(event):
   await event.reply("**Ali'nin Botu**, Selam Ben Sana Beni Tanıtıcam!★\nBeni tanımak için **/ali**'i tıklayın.",
                     reply_markup=InlineKeyboardMarkup(
                 [
@@ -25,7 +24,6 @@ async def start(event):
                     ]
                 ]
 @client.on(events.NewMessage(pattern="^/ali$"))
-async def start(event):
   await event.reply("**Ali Kimdir**, Ali Sadece Kendini Geliştirmeye Çalışan Biri Azimli Bir Şekilde İlerliyor\nDaha fazla bilgi için Aşağıdaki Butonlardan Ali'nin kanal ve grupların akatılabilir botlarını kullanabilirsin eğer gruba girersen onunla birebir sohbet edebilirsin.",
                     reply_markup=InlineKeyboardMarkup(
                 [
