8973ef311476dd70cc9b75c335a295b1fe5a267d
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2021-12-08 23:23:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,17 +16,22 @@ app = Client(
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply("**Ali'nin Botu**, Selam Ben Sana Beni Tanıtıcam!★\nBeni tanımak için **/ali**'i tıklayın.",
-                    reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton(text="Bot Sahibi", url=f"https://t.me/mmagneto")]
-                ])
+                    reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Sahip", url="https://t.me/mmagneto"
+                        )
+                    ]
+                ]
 @client.on(events.NewMessage(pattern="^/ali$"))
 async def start(event):
   await event.reply("**Ali Kimdir**, Ali Sadece Kendini Geliştirmeye Çalışan Biri Azimli Bir Şekilde İlerliyor\nDaha fazla bilgi için Aşağıdaki Butonlardan Ali'nin kanal ve grupların akatılabilir botlarını kullanabilirsin eğer gruba girersen onunla birebir sohbet edebilirsin.",
-                    reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton(text="Marvel Sohbet", url=f"https://t.me/sohbetgnl"), 
-                    InlineKeyboardButton(text="Film Kanalı", url=f"https://t.me/quickwaste"), 
-                    InlineKeyboardButton(text="Tagger Bot", url=f"https://t.me/alitaggerbot"), 
-                    InlineKeyboardButton(text="Film Botu", url=f"https://t.me/quickwastebot"), 
-                    InlineKeyboardButton(text="Kişisel Blog", url=f"https://t.me/mmagneto3"), 
+                    reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Marvel Sohbet", url="https://t.me/sohbetgnl"
+                        )
                     ]
-                ])
+                ]
