69eb2890a37bf49035affc0e27d2607e3d90ca59
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2021-12-09 19:38:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,19 +14,17 @@ app = Client(
     api_hash=api_hash
 )
 @client.on(events.NewMessage(pattern="^/start$"))
-async def help(event):
-  helptext = "**Ali'nin Botu**, Selam Ben Sana Beni Tanıtıcam!★\nBeni tanımak için **/mmagneto**'i tıklayın."
-  await event.reply("helptext,
+async def start(event):
+  await event.reply("**Ali'nin Botu**, Selam Ben Sana Beni Tanıtıcam!★\nBeni tanımak için **/mmagneto**'i tıklayın.",
                                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Sahibim", url=f"https://t.me/mmagneto")]]),
-                             disable_web_page_preview=True")
+                             disable_web_page_preview=True
 
 @client.on(events.NewMessage(pattern="^/mmagneto$"))
-async def help(event):
-  mmtext = "**Ali Kimdir**, Ali Sadece Kendini Geliştirmeye Çalışan Biri Azimli Bir Şekilde İlerliyor\nDaha fazla bilgi için Aşağıdaki Butonlardan Ali'nin kanal ve grupların akatılabilir botlarını kullanabilirsin eğer gruba girersen onunla birebir sohbet edebilirsin." 
-  await event.reply("mmtext,
+async def mmagneto(event):
+  await event.reply("**Ali Kimdir**, Ali Sadece Kendini Geliştirmeye Çalışan Biri Azimli Bir Şekilde İlerliyor\nDaha fazla bilgi için Aşağıdaki Butonlardan Ali'nin kanal ve grupların akatılabilir botlarını kullanabilirsin eğer gruba girersen onunla birebir sohbet edebilirsin.",
                                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Marvel Sohbet", url=f"https://t.me/sohbetgnl"),
                                                                  InlineKeyboardButton("Film Kanalı", url=f"https://t.me/quickwaste"),
                                                                  InlineKeyboardButton("Film Botu", url=f"https://t.me/quickwastebot"),
                                                                  InlineKeyboardButton("Tagger Bot", url=f"https://t.me/alitaggerbot"),
                                                                  InlineKeyboardButton("Kişisel Blog", url=f"https://t.me/mmagneto3")]]),
-                             disable_web_page_preview=True") 
+                             disable_web_page_preview=True
