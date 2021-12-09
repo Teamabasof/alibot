8fd10d646a472ca76ed9858b62a587aa4f867288
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2021-12-09 18:59:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,12 +14,12 @@ app = Client(
     api_hash=api_hash
 )
 @client.on(events.NewMessage(pattern="^/start$"))
-  await event.reply("**Ali'nin Botu**, Selam Ben Sana Beni Tanıtıcam!★\nBeni tanımak için **/mmagneto**'i tıklayın.",
+  await event.reply("**Ali'nin Botu**, Selam Ben Sana Beni Tanıtıcam!★\nBeni tanımak için **/mmagneto**'i tıklayın."),
                                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Sahibim", url=f"https://t.me/mmagneto")]]),
                              disable_web_page_preview=True
 
 @client.on(events.NewMessage(pattern="^/mmagneto$"))
-  await event.reply("**Ali Kimdir**, Ali Sadece Kendini Geliştirmeye Çalışan Biri Azimli Bir Şekilde İlerliyor\nDaha fazla bilgi için Aşağıdaki Butonlardan Ali'nin kanal ve grupların akatılabilir botlarını kullanabilirsin eğer gruba girersen onunla birebir sohbet edebilirsin.",
+  await event.reply("**Ali Kimdir**, Ali Sadece Kendini Geliştirmeye Çalışan Biri Azimli Bir Şekilde İlerliyor\nDaha fazla bilgi için Aşağıdaki Butonlardan Ali'nin kanal ve grupların akatılabilir botlarını kullanabilirsin eğer gruba girersen onunla birebir sohbet edebilirsin."),
                                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Marvel Sohbet", url=f"https://t.me/sohbetgnl"),
                                                                  InlineKeyboardButton("Film Kanalı", url=f"https://t.me/quickwaste"),
                                                                  InlineKeyboardButton("Film Botu", url=f"https://t.me/quickwastebot"),
